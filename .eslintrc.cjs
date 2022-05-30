644c7332fc8463949a77bcc4a3c9e325a36fe47f
manura18 <mary.kondreskul@gmail.com>
2022-05-30 13:15:20 +0300

    chore: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+  rules: {
+    // prettier
+    "prettier/prettier": [
+      "warn",
+      {
+        tabWidth: 2,
+        singleQuote: true,
+        semi: false,
+        trailingComma: "es5",
+      },
+    ],
+  },
+};
