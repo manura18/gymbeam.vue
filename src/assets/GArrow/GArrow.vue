7992a796a8223aa709d398e586eb97918e8472e9
manura18 <mary.kondreskul@gmail.com>
2022-06-15 13:10:45 +0300

    feat: add GArrow
    git push

---
+++ src/assets/GArrow/GArrow.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+    fill="rgba(255,255,255)"
+    viewBox="0 0 28 28"
+  >
+    <path d="M20.686 16l-9.257 12.914v-3.086l7.086-9.829-7.086-9.829v-3.086z" />
+    <path
+      d="M21.829 16l-11.314 15.771v-6.286l6.857-9.486-6.857-9.486v-6.286z"
+    />
+  </svg>
+</template>
