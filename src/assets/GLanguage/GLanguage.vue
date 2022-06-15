0c7c2ab5fec56677a1af8f6ab0621e1e92f41c32
manura18 <mary.kondreskul@gmail.com>
2022-06-15 13:17:38 +0300

    feat: add GLanguage

---
+++ src/assets/GLanguage/GLanguage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <svg
+    class="mt-1 mr-2 w-[25px]"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 640 480"
+  >
+    <g fill-rule="evenodd" stroke-width="1pt">
+      <path fill="#ffd500" d="M0 0h640v480H0z" />
+      <path fill="#005bbb" d="M0 0h640v240H0z" />
+    </g>
+  </svg>
+</template>
---
+++ src/assets/GLanguage/index.ts
@@ -0,0 +1,1 @@
+export { default as GLanguage } from './GLanguage.vue'
