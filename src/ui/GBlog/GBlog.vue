cb4a0e94006ce8a953c0c548972202b9ef7063f7
manura18 <mary.kondreskul@gmail.com>
2022-07-06 23:43:40 +0300

    feat(ui): add GBlog

---
+++ src/ui/GBlog/GBlog.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="w-[300px] h-[360px]">
+    <a href="">
+      <div>
+        <img
+          style="
+            display: block;
+            -webkit-user-select: none;
+            margin: auto;
+            background-color: hsl(0, 0%, 90%);
+            transition: background-color 300ms;
+          "
+          src="https://gymbeam.ua/blog/wp-content/uploads/2022/03/featured-tiramisu-400x209.png"
+        />
+      </div>
+      <div class="font-bold">
+        <span
+          >Фитнес рецепт: Восхитительный тирамису с высоким содержанием
+          белка.</span
+        >
+      </div>
+      <p class="text-sm">
+        Вы фанат итальянской кухни и кофейных десертов? Тогда вам точно стоит
+        приготовить этот восхитительный десерт тирамису. Кроме его вкуса и
+        кремовой консистенции, он также порадует вас высоким содержанием белка.
+      </p>
+    </a>
+  </div>
+</template>
---
+++ src/ui/GBlog/index.ts
@@ -0,0 +1,1 @@
+export { default as GBlog } from './GBlog.vue'
