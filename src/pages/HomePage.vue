a07f93db15f8aaf51106abf11bc16c65fa4a993b
manura18 <mary.kondreskul@gmail.com>
2022-10-07 15:55:13 +0300

    feat(homepage): ui
    HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,39 @@
+<script setup lang="ts">
+import { GHeader } from '@/ui/GHeader'
+import { GCategoryCard } from '@/shared/ui/GCategoryCard'
+import { GSwiper } from '@/shared/ui/GSwiper'
+import { GFooter } from '@/ui/GFooter'
+import { GGood } from '@/shared/ui/GGood'
+import { GBlog } from '@/ui/GBlog'
+import { goods } from '@/pages/goods'
+import { blogs } from '@/pages/blog'
+</script>
+
 <template>
-  <main>
-    <h1>Hello Manura!</h1>
-  </main>
+  <GHeader class="z-10" />
+  <div class="flex flex-col items-center">
+    <GSwiper class="z-0" />
+    <GCategoryCard class="justify-center items-center pt-10 mx-auto" />
+    <div class="flex flex-wrap max-w-[940px]">
+      <GGood
+        v-for="(good, i) in goods"
+        :title="good.title"
+        :image="good.image"
+        :rating="good.rating"
+        :commentsCount="good.commentsCount"
+        :price="good.price"
+        :key="i"
+      />
+    </div>
+    <div class="flex max-w-[940px]">
+      <GBlog
+        v-for="(blog, i) in blogs"
+        :image="blog.image"
+        :title="blog.title"
+        :text="blog.text"
+        :key="i"
+      />
+    </div>
+  </div>
+  <GFooter />
 </template>
---
+++ src/pages/goods.ts
@@ -0,0 +1,67 @@
+import moxy from '@/assets/moxy.png'
+import vitality from '@/assets/vitality.png'
+import vitamin from '@/assets/vitamin.png'
+import whey from '@/assets/whey.png'
+import collaPink from '@/assets/collaPink.png'
+import proteinMilky from '@/assets/proteinMilky.png'
+import fuegain from '@/assets/fuegain.png'
+import protein from '@/assets/protein.png'
+
+export const goods = [
+  {
+    title: 'Напій',
+    image: moxy,
+    rating: 95,
+    commentsCount: 83,
+    price: 45,
+  },
+  {
+    title: 'Вітаміни',
+    image: vitality,
+    rating: 90,
+    commentsCount: 58,
+    price: 145,
+  },
+  {
+    title: 'Мультивітамінний',
+    image: vitamin,
+    rating: 80,
+    commentsCount: 65,
+    price: 245,
+  },
+  {
+    title: 'Протеїн',
+    image: whey,
+    rating: 85,
+    commentsCount: 74,
+    price: 458,
+  },
+  {
+    title: 'Напій',
+    image: collaPink,
+    rating: 95,
+    commentsCount: 83,
+    price: 45,
+  },
+  {
+    title: 'Напій',
+    image: proteinMilky,
+    rating: 28,
+    commentsCount: 64,
+    price: 250,
+  },
+  {
+    title: 'Напій',
+    image: fuegain,
+    rating: 35,
+    commentsCount: 33,
+    price: 450,
+  },
+  {
+    title: 'Напій',
+    image: protein,
+    rating: 55,
+    commentsCount: 63,
+    price: 420,
+  },
+]
