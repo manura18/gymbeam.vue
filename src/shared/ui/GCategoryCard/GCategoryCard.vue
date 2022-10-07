2ce512b6cbd600122a21d37f5a7a8157563b2527
manura18 <mary.kondreskul@gmail.com>
2022-10-07 15:32:07 +0300

    fix(gcategorycard): repair layout

--- src/shared/ui/GCategoryCard/GCategoryCard.vue
+++ src/shared/ui/GCategoryCard/GCategoryCard.vue
@@ -7,40 +7,42 @@ import { links } from './links'
 </script>
 
 <template>
-  <div class="flex justify-start pb-6 max-w-[940px]">
-    <img class="pb-6 w-[30px]" :src="heart" />
-    <div class="mr-3 ml-2">
-      <span class="text-sm font-bold">6мл+</span>
-      <p class="text-[13px]">задоволених замовників</p>
+  <div class="max-w-[940px]">
+    <div class="flex pb-6">
+      <img class="pb-6 w-[30px]" :src="heart" />
+      <div class="mr-3 ml-2">
+        <span class="text-sm font-bold">6мл+</span>
+        <p class="text-[13px]">задоволених замовників</p>
+      </div>
+      <img class="pb-6 w-[20px]" :src="hourglass" />
+      <div class="mr-3 ml-2">
+        <span class="text-sm font-bold">До 4 днів</span>
+        <p class="text-[13px]">швидка доставка</p>
+      </div>
+      <img class="pb-7 w-[35px]" :src="truck" />
+      <div class="mr-3 ml-2 text-sm">
+        <span class="font-bold">Безкоштовна доставка</span>
+        <p class="text-[13px]">для замовлень на суму від 1400 грн.</p>
+      </div>
+      <img class="pb-[22px] w-[30px]" :src="box" />
+      <div class="ml-2 text-sm">
+        <span class="font-bold">5000+ продуктів</span>
+        <p class="text-[13px]">найширший асортимент</p>
+      </div>
     </div>
-    <img class="pb-6 w-[20px]" :src="hourglass" />
-    <div class="mr-3 ml-2">
-      <span class="text-sm font-bold">До 4 днів</span>
-      <p class="text-[13px]">швидка доставка</p>
+    <div class="pb-3">
+      <span class="font-bold"
+        >Откройте для себя проверенные продукты, которые помогут вам достичь
+        ваших целей. Ваша цель:</span
+      >
     </div>
-    <img class="pb-7 w-[35px]" :src="truck" />
-    <div class="mr-3 ml-2 text-sm">
-      <span class="font-bold">Безкоштовна доставка</span>
-      <p class="text-[13px]">для замовлень на суму від 1400 грн.</p>
+    <div class="inline-grid grid-cols-4 gap-4 max-w-[940px]">
+      <a class="flex pr-20 bg-[#e9e9e9]" v-for="(link, i) in links" :key="i">
+        <img class="w-[75px]" :src="link.image" />
+        <p class="pl-2 m-auto text-base font-bold leading-[15px] text-[#666]">
+          {{ link.title }}
+        </p>
+      </a>
     </div>
-    <img class="pb-[22px] w-[30px]" :src="box" />
-    <div class="ml-2 text-sm">
-      <span class="font-bold">5000+ продуктів</span>
-      <p class="text-[13px]">найширший асортимент</p>
-    </div>
-  </div>
-  <div class="pb-3">
-    <span class="font-bold"
-      >Откройте для себя проверенные продукты, которые помогут вам достичь ваших
-      целей. Ваша цель:</span
-    >
-  </div>
-  <div class="inline-grid grid-cols-4 gap-4 max-w-[940px]">
-    <a class="flex pr-20 bg-[#e9e9e9]" v-for="(link, i) in links" :key="i">
-      <img class="w-[75px]" :src="link.image" />
-      <p class="pl-2 m-auto text-base font-bold leading-[15px] text-[#666]">
-        {{ link.title }}
-      </p>
-    </a>
   </div>
 </template>
